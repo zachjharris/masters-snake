be9e5ea1d63dc44c0d1815b6ee315fdfb5ebd707
zachjharris <zachjharris@gmail.com>
2022-04-08 09:01:49 -0400

    797fbd1

---
+++ precache-manifest.3a1a029ab57978aa878ef8377a887a3c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97cc9eb6dcb63d27cade056e912c4f7e",
+    "url": "https://zachjharris.github.io/masters-snake/apple-touch-icon-152x152.jpg"
+  },
+  {
+    "revision": "b58986d7d4e543234fdf",
+    "url": "https://zachjharris.github.io/masters-snake/css/app.f7532203.css"
+  },
+  {
+    "revision": "38c4c3967128ba18fff3",
+    "url": "https://zachjharris.github.io/masters-snake/css/chunk-vendors.c8028384.css"
+  },
+  {
+    "revision": "734ed5c4896fdc9fcc127ec413d73983",
+    "url": "https://zachjharris.github.io/masters-snake/favicon-16x16.png"
+  },
+  {
+    "revision": "155715005fb7892a003cda4d94c56b8b",
+    "url": "https://zachjharris.github.io/masters-snake/favicon-32x32.png"
+  },
+  {
+    "revision": "b10e741139d20b9d2df7956e0d505102",
+    "url": "https://zachjharris.github.io/masters-snake/img/masters_logo.b10e7411.png"
+  },
+  {
+    "revision": "60cf6b6f0df92437e08e3cd36a46d775",
+    "url": "https://zachjharris.github.io/masters-snake/index.html"
+  },
+  {
+    "revision": "b58986d7d4e543234fdf",
+    "url": "https://zachjharris.github.io/masters-snake/js/app.314d7bc1.js"
+  },
+  {
+    "revision": "38c4c3967128ba18fff3",
+    "url": "https://zachjharris.github.io/masters-snake/js/chunk-vendors.20066c54.js"
+  },
+  {
+    "revision": "bfbefa3d51343298a171bb1871ecad86",
+    "url": "https://zachjharris.github.io/masters-snake/logo.png"
+  },
+  {
+    "revision": "07d1e22e8cddd99a53037dc510221b7b",
+    "url": "https://zachjharris.github.io/masters-snake/logo.svg"
+  },
+  {
+    "revision": "0f55ee201d3a117cd37f8db996b4901f",
+    "url": "https://zachjharris.github.io/masters-snake/manifest.json"
+  },
+  {
+    "revision": "b10e741139d20b9d2df7956e0d505102",
+    "url": "https://zachjharris.github.io/masters-snake/masters_logo.png"
+  }
+]);
